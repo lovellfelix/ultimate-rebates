041b959da93b8ed99748bed17adbd891650ec985
Lovell Felix <lovell.felix@gmail.com>
2015-05-15 07:31:09 -0500

    WIP: Implementing feature to update on the fly

---
+++ modules/settings/server/controllers/update.server.controller.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash'),
+    path = require('path'),
+    Update = require('simple-git')( __dirname );
+
+/**
+ * Create a Update
+ */
+exports.create = function(req, res) {
+
+};
+
+/**
+ * Show the current Update
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Update
+//  */
+// exports.update = function(req, res) {
+//
+// };
+
+/**
+ * Delete an Update
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Updates
+ */
+exports.list = function(req, res) {
+
+};
+
+exports.update1 = function() {
+
+  console.log('Hello world');
+
+};
